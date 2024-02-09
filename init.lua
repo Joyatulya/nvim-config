d9ecb6fc47abf1465f89f6c0dce7641b476edb11
Joy <joy.atulya@gmail.com>
2024-02-09 12:40:55 +0000

    Added default color scheme

--- init.lua
+++ init.lua
@@ -200,8 +200,19 @@ require('lazy').setup({
     },
   },
 
-  'folke/tokyonight.nvim',
-
+  { 'folke/tokyonight.nvim' , init = function ()
+    vim.cmd.colorscheme('tokyonight')
+  end},
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+},
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
