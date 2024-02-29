e9f73d7135b929783ab595c578b0f39c9a494fbc
Joyatuyla <joy.atulya@gmail.com>
2024-02-29 18:53:37 +0000

    fix: Search functionality on colemak

--- lua/colemak-bindings.lua
+++ lua/colemak-bindings.lua
@@ -1,8 +1,7 @@
 local mode = {'n', 'o', 'x'}
+vim.keymap.set(mode, 'h', 'n', {noremap = true})
 vim.keymap.set(mode, 'n', 'j', {})
-vim.keymap.set(mode, 'N', 'J', {})
 vim.keymap.set(mode, 'e', 'k', {})
-vim.keymap.set(mode, 'E', 'K', {})
 vim.keymap.set(mode, 'm', 'h', {})
 vim.keymap.set(mode, 'M', 'H', {})
 -- To enter the insert mode
